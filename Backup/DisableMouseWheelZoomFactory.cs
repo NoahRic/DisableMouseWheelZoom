3c100f37ab79fff1bf5c9465ff318a5594eaa1d5
Giovanni Lambiase <giovanni@glat.it>
2016-11-17 14:19:43 +0100

    Solution updated to be compiled on VS2015, extension updated to be installed on VS2017

---
+++ Backup/DisableMouseWheelZoomFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace DisableMouseWheelZoom
+{
+    [Export(typeof(IWpfTextViewCreationListener ))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Zoomable)]
+    class ViewCreationListener : IWpfTextViewCreationListener
+    {
+        public void  TextViewCreated(IWpfTextView textView)
+        {
+            textView.Options.SetOptionValue(DefaultWpfViewOptions.EnableMouseWheelZoomId, false);
+        }
+    }
+}
